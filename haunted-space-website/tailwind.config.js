b6ead187a468491929d777b94a41b32dffa28e38
pietrociatta <59965830+pietrociatta@users.noreply.github.com>
2022-04-29 18:28:28 +0200

    Setup react app and addedd tailwind

---
+++ haunted-space-website/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  mode: "jit",
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  purge: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
